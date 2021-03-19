5bca59efc97085daa9208262bf8cef856fa6eef1
DJREMiX6 <35576682+DJREMiX6@users.noreply.github.com>
2021-03-19 17:04:33 +0100

    Aggiungere i file di progetto.

---
+++ BookListMVC/Controllers/BooksController.cs
@@ -0,0 +1,91 @@
+﻿using BookListMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListMVC.Controllers
+{
+    public class BooksController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public BooksController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            Book = new Book();
+            if(id != null)
+            {
+                //Update request
+                Book = _db.Books.FirstOrDefault(x => x.Id == id);
+                if(Book == null)
+                {
+                    return NotFound();
+                }
+                return View(Book);
+            }
+            //Create request
+            return View(Book);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert()
+        {
+            if(ModelState.IsValid)
+            {
+                if(Book.Id == 0)
+                {
+                    //Create request
+                    _db.Books.Add(Book);
+                }
+                else
+                {
+                    _db.Books.Update(Book);
+                }
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(Book);
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Books.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            Book bookFromDb = await _db.Books.FirstOrDefaultAsync(x => x.Id == id);
+            if (bookFromDb == null)
+            {
+                return Json(new { success = false, message = "Error! Book not found!" });
+            }
+            _db.Books.Remove(bookFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Book Deleted succesfully!" });
+        }
+
+        #endregion API CALLS
+
+    }
+}
---
+++ BookListMVC/wwwroot/js/bookList.js
@@ -0,0 +1,68 @@
+﻿var dataTable;
+
+
+$(document).ready(function () {
+    loadDataTable();
+})
+
+function loadDataTable() {
+    dataTable = $('#DT_load').dataTable({
+        ajax: {
+            "url": "/Books/GetAll/",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "25%" },
+            { "data": "author", "width": "25%" },
+            { "data": "isbn", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center" style="margin: 0 auto; padding: 0px;">\
+                                <a href="/Books/Upsert?id=${data}" class="btn btn-success text-white" style="cursor: pointer; width: 40%; margin: 0 auto; margin-right: 5%;">Edit</a>\
+                                <a class="btn btn-danger text-white" onclick="DeleteBook(${data})" style="cursor: pointer; width: 40%; margin: 0 auto; margin-left: 5%;">Delete</a>\
+                            </div>`;
+                },
+                "width": "35%"
+            }
+        ],
+        "language": {
+            "emptyTable": "no data found"
+        },
+        "width": "100%"
+    })
+}
+
+function DeleteBook(id) {
+    let url = "/Books/Delete?id=" + id;
+    swal({
+        title: "Are you sure?",
+        text: "You can not undo this action!",
+        buttons: {
+            cancel: true,
+            confirm: true
+        },
+        icon: "warning",
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    toastr.options.closeButton = true;
+                    toastr.options.newestOnTop = true;
+                    toastr.options.progressBar = true;
+                    toastr.options.timeOut = 1000;
+                    if (data.success) {
+                        dataTable.api().ajax.reload();
+                        toastr.success(data.message);
+                    } else {
+                        toastr.error(data.message);
+                    }
+                }
+            })
+        }
+    })
+}
